03a6b0e5285a66c7350431ab5afd703aa901daaf
Abdul Rehman <145242032+digitalrehman@users.noreply.github.com>
2025-08-08 23:01:30 -0700

    Update DashboardTabs.tsx

--- src/components/DashboardTabs.tsx
+++ src/components/DashboardTabs.tsx
@@ -1,26 +1,41 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { responsiveHeight } from '../utils/Responsive';
 import { APPCOLORS } from '../utils/APPCOLORS';
 import AppText from './AppText';
+import Feather from 'react-native-vector-icons/Feather';
 
 type Props = {
-  logo?: any;
   name?: string;
-  isNew?: boolean; // changed 'new' to 'isNew'
-  onPress?: () => void,
-  img: any
+  isNew?: boolean;
+  onPress?: () => void;
+  icon: string;
 };
 
-const DashboardTabs = ({ logo, name, isNew,onPress, img }: Props) => {
+const DashboardTabs = ({ name, onPress, icon }: Props) => {
   return (
-    <TouchableOpacity onPress={onPress} style={{height:responsiveHeight(12), width:responsiveHeight(12), backgroundColor:APPCOLORS.WHITE, borderRadius:20, alignItems:'center', justifyContent:'center', gap:10, paddingHorizontal:15}}>
-      <Image source={img} style={{height:responsiveHeight(4), width:responsiveHeight(4)}}/>
-      <AppText title={name} titleAlignment={"center"} titleColor={APPCOLORS.BLACK} titleSize={1.7} titleWeight/>
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        height: responsiveHeight(12),
+        width: responsiveHeight(12),
+        backgroundColor: APPCOLORS.WHITE,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 10,
+        paddingHorizontal: 15,
+      }}>
+      <Feather name={icon} size={30} color="black" />
+      <AppText
+        title={name}
+        titleAlignment={'center'}
+        titleColor={APPCOLORS.BLACK}
+        titleSize={1.5}
+        titleWeight
+      />
     </TouchableOpacity>
   );
 };
-
-                  
 
 export default DashboardTabs;
